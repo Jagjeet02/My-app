85a0d078d5987b358b996e003e2b89283372181d
Jagjeet02 <jagjeetpanesar000@gmail.com>
2025-09-04 22:47:25 +0530

    sign page ui done

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+// "use client";
+// import Link from "next/link";
+
+// export default function Signup() {
+//     return(
+//         <div className="signup_container">
+//             <form action="">
+//                 <Link href="../welcome"></Link>
+//             </form>
+//         </div>
+//     );
+// }
---
+++ app/welcome/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      Welocome
+    </div>
+  )
+}
+
+export default page
